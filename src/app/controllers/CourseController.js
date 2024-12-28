9dcfdecf6f5ba9443035e187e1eefaf1ad788b4c
Mai Van Thi <poiseidoncoder@gmail.com>
2024-12-28 20:37:23 +0700

    Add Course Controller

---
+++ src/app/controllers/CourseController.js
@@ -0,0 +1,26 @@
+const Course = require('../models/Course');
+const { mongooseToObject } = require('../../utils/mongoose');
+class CourseController {
+  //[Get] /courses/:slug
+  show(req,res,next){
+    Course.findOne({ slug: req.params.slug })
+      .then(course => {
+        res.render('courses/show', { course: mongooseToObject(course) });
+      })
+      .catch(next); 
+  }
+  create(req,res,next){
+    res.render('courses/create');
+  }
+  //[Post] /courses/store
+  store(req,res,next){
+    const formData = req.body;
+    formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+    const course = new Course(req.body);
+     course.save().then(
+      () => res.redirect(`/`) 
+     );
+  }
+}
+
+module.exports = new CourseController();
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const newsRouter = require('./news');
 const siteRouter = require('./site');
+const coursesRouter = require('./courses');
 
 function route(app){
   // Định nghĩa route cho trang chủ
   app.use('/news',newsRouter);
+  app.use('/courses', coursesRouter);
   app.use('/', siteRouter);
 
 }
